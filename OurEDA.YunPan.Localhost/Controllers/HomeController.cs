321d752aaf81c2701a456e9fab978c167b6a5bbb
ximing <morningxm@hotmail.com>
2014-10-28 16:34:50 +0800

    1

--- OurEDA.YunPan.Localhost/Controllers/HomeController.cs
+++ OurEDA.YunPan.Localhost/Controllers/HomeController.cs
@@ -84,6 +84,92 @@ namespace MvcWebRole1.Controllers
             }
         }
 
+        public void DownPublicFileByrandName(string randName)
+        {
+            if (string.IsNullOrEmpty(randName))
+            {
+                TempData["error"] = "提取码不能为空";
+                Response.Redirect("Public");
+                return;
+            }
+            var database = server.GetDatabase("test");
+            var downcollection = database.GetCollection<FileStore>("filestores");
+            var fileinfo = downcollection.FindOne(Query.EQ("RandName", randName));
+            if (fileinfo == null)
+            {
+                TempData["error"] = "提取码不存在";
+                Response.Redirect("Public");
+                return;
+            }
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(fileinfo.FileUrl);
+            if (fileInfo.Exists == true)
+            {
+                const long ChunkSize = 51000;
+                byte[] buffer = new byte[ChunkSize];
+                Response.Clear();
+                System.IO.FileStream iStream = System.IO.File.OpenRead(fileinfo.FileUrl);
+                long dataLengthToRead = iStream.Length;//获取下载的文件总大小
+                Response.ContentType = "application/octet-stream";
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + HttpUtility.UrlEncode(fileinfo.FileName.Substring(4)));
+                Response.AddHeader("Content-Length", iStream.Length.ToString());
+                while (dataLengthToRead > 0 && Response.IsClientConnected)
+                {
+                    int lengthRead = iStream.Read(buffer, 0, Convert.ToInt32(ChunkSize));
+                    Response.OutputStream.Write(buffer, 0, lengthRead);
+                    Response.Flush();
+                    dataLengthToRead = dataLengthToRead - lengthRead;
+                }
+                Response.Close();
+            }
+            else
+            {
+                TempData["error"] = "文件不存在";
+                Response.Redirect("Public");
+            }
+        }
+
+        public ActionResult AddPublic(string randName)
+        {
+            if (string.IsNullOrEmpty(randName))
+            {
+                TempData["error"] = "提取码不能为空";
+                return View("Index"); ;
+            }
+            var database = server.GetDatabase("test");
+            var downcollection = database.GetCollection<FileStore>("filestores");
+            var fileinfo = downcollection.FindOne(Query.EQ("RandName", randName));
+            if (fileinfo == null)
+            {
+                TempData["error"] = "提取码不存在";
+                return View("Index");
+            }
+            else
+            {
+                using (OurEDA.YunPan.Localhost.db.YunPanEntities db = new OurEDA.YunPan.Localhost.db.YunPanEntities())
+                {
+                     System.IO.FileInfo fileInfo = new System.IO.FileInfo(fileinfo.FileUrl);
+                     db.publicFiles.Add(new OurEDA.YunPan.Localhost.db.publicFile
+                     {
+                         CreateTime = DateTime.Now,
+                         DownCount = 1,
+                         fileSize = fileInfo.Length,
+                         LikeCount = 1,
+                         Name = fileinfo.FileName.Substring(4),
+                         RandName = fileinfo.RandName
+                     });
+                     db.SaveChanges();
+                    return RedirectToAction("Public");
+                }
+            }
+        }
+
+        public ActionResult Public()
+        {
+            using (OurEDA.YunPan.Localhost.db.YunPanEntities db = new OurEDA.YunPan.Localhost.db.YunPanEntities())
+            {
+                return View(db.publicFiles.ToList());
+            }
+        }
 
 
 
